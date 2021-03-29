8eee651b534e1616be6ea21ae5508a12748c5a83
leejunsu <qkfhtit@gmail.com>
2021-03-29 17:08:24 +0900

    first commit

---
+++ src/main/kotlin/com/todo/reacttodo/ReacttodoApplication.kt
@@ -0,0 +1,11 @@
+package com.todo.reacttodo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReacttodoApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReacttodoApplication>(*args)
+}
---
+++ src/test/kotlin/com/todo/reacttodo/ReacttodoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.todo.reacttodo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReacttodoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
